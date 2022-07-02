1579258e1a99ed9967236c29e8ef69f3509e1986
0xMoad <106631897+0xMoad@users.noreply.github.com>
2022-07-03 00:33:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 let addToDo = document.getElementById("addToDo");
 let inputField = document.getElementById("inputField");
 let container = document.getElementById("toDoContainer")
+let reset = document.getElementById("reset")
 
 addToDo.addEventListener("click", function(){
+    
     if(inputField.value !== ""){
         let paragraph = document.createElement("li");
     paragraph.classList.add('paragraph-styling');
     paragraph.innerHTML = inputField.value;
     container.appendChild(paragraph);
     inputField.value = "";
-    
+
+    reset.addEventListener("click", function(){
+        container.innerHTML = ""
+    })
 
     paragraph.addEventListener("click", function (){
         paragraph.style.textDecoration = "line-through"
@@ -20,6 +25,8 @@ addToDo.addEventListener("click", function(){
         container.removeChild(paragraph);
 
     })
+
+
     
 } else {
     alert("You need to submit a task :)")
